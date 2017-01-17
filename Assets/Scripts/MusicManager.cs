f914ae231351ee62da2bac67ef6b5b21c6fad2bc
Goga Akobiya <googlichek.one@gmail.com>
2017-01-17 19:33:00 +0300

    glitch-garden: Added alternative music player logic.

---
+++ Assets/Scripts/MusicManager.cs
@@ -0,0 +1,34 @@
+using System.Xml.Serialization;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicManager : MonoBehaviour
+{
+    public AudioClip[] LevelSoundArray;
+
+    private AudioSource _audioSource;
+
+    private void OnSceneLoad(Scene scene, LoadSceneMode mode)
+    {
+        var thisSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        var currentLevelSound = LevelSoundArray[thisSceneIndex];
+        _audioSource = GetComponent<AudioSource>();
+
+        if (currentLevelSound)
+        {
+            _audioSource.clip = currentLevelSound;
+            _audioSource.loop = true;
+            _audioSource.Play();
+        }
+    }
+
+    void Start()
+    {
+        SceneManager.sceneLoaded += OnSceneLoad;
+    }
+
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+}
