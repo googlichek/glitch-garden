62ccbe2580a545749e3e5c4835c404abb195842d
Goga Akobiya <googlichek.one@gmail.com>
2017-01-31 13:13:06 +0300

    glitch-garden: Adding colliders on game units.

--- Assets/Scripts/Attacker.cs
+++ Assets/Scripts/Attacker.cs
@@ -5,8 +5,19 @@ public class Attacker : MonoBehaviour
     [Range(-1.5f, 1.5f)]
     public float WalkSpeed;
 
+    void Start()
+    {
+        Rigidbody2D rigidbody = gameObject.AddComponent<Rigidbody2D>();
+        rigidbody.isKinematic = true;
+    }
+
     void Update()
     {
         transform.Translate(Vector3.left * WalkSpeed * Time.deltaTime);
+    }
+
+    void OnTriggerEnter2D()
+    {
+        
     }
 }
