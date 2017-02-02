596e719d9f19fbe99d3dc79047a6b9ff7fb61db7
Goga Akobiya <googlichek.one@gmail.com>
2017-02-02 23:56:58 +0300

    glitch-garden: Redoing animations.

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    public float Speed;
+    public float Damage;
+
+    void Update()
+    {
+        transform.Translate(Vector3.right * Speed * Time.deltaTime);
+    }
+}
