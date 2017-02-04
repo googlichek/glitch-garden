c47e7b4ea644ab2c09b0dd290d052a2dcac7970f
Goga Akobiya <googlichek.one@gmail.com>
2017-02-04 23:23:26 +0300

    glitch-garden: Projectiles now deal damage.

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -9,4 +9,16 @@ public class Projectile : MonoBehaviour
     {
         transform.Translate(Vector3.right * Speed * Time.deltaTime);
     }
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        Attacker attacker = collider.gameObject.GetComponent<Attacker>();
+        Health health = collider.gameObject.GetComponent<Health>();
+
+        if (attacker && health)
+        {
+            health.DealDamage(Damage);
+            Destroy(gameObject);
+        }
+    }
 }
