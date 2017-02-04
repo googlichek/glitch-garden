3ffbe94f6c0e6522e463552bd41f1c97110b5e03
Goga Akobiya <googlichek.one@gmail.com>
2017-02-04 22:51:48 +0300

    glitch-garden: Preparing ground for defender units to properly defend themselves.

--- Assets/Scripts/Defender.cs
+++ Assets/Scripts/Defender.cs
@@ -2,13 +2,4 @@ using UnityEngine;
 
 public class Defender : MonoBehaviour
 {
-	// Use this for initialization
-	void Start ()
-    {
-	}
-	
-	// Update is called once per frame
-	void Update ()
-    {
-	}
 }
---
+++ Assets/Scripts/Shooter.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class Shooter : MonoBehaviour
+{
+    public GameObject Projectile;
+    public GameObject ProjectileParent;
+    public GameObject Gun;
+
+    private void Fire()
+    {
+        GameObject projectile = Instantiate(Projectile) as GameObject;
+        projectile.transform.parent = ProjectileParent.transform;
+        projectile.transform.position = Gun.transform.position;
+    }
+}
