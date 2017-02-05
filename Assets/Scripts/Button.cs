d31605aeace4b5939c0e871a689bd4a337ba3452
Goga Akobiya <googlichek.one@gmail.com>
2017-02-05 23:11:28 +0300

    glitch-garden: Preparing ground for placing defenders on scene by choice.

---
+++ Assets/Scripts/Button.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class Button : MonoBehaviour
+{
+    public static GameObject _selectedDefender;
+    public GameObject DefenderPrefab;
+
+    private Button[] _buttonArray;
+
+    void Start()
+    {
+        _buttonArray = FindObjectsOfType<Button>();
+    }
+
+    void OnMouseDown()
+    {
+        foreach (Button button in _buttonArray)
+        {
+            button.GetComponent<SpriteRenderer>().color = Color.black;
+        }
+
+        GetComponent<SpriteRenderer>().color = Color.white;
+        _selectedDefender = DefenderPrefab;
+    }
+}
