ffb49c72215cc6b53fff656a420e7499be3e7c9e
Goga Akobiya <googlichek.one@gmail.com>
2017-02-06 00:33:17 +0300

    glitch-garden: Attaching projectiles to proper object in hierarchy from script.

--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -3,13 +3,24 @@
 public class Shooter : MonoBehaviour
 {
     public GameObject Projectile;
-    public GameObject ProjectileParent;
     public GameObject Gun;
+
+    private GameObject _projectileParent;
+
+    void Start()
+    {
+        _projectileParent = GameObject.Find("Projectiles");
+
+        if (!_projectileParent)
+        {
+            _projectileParent = new GameObject("Projectiles");
+        }
+    }
 
     private void Fire()
     {
         GameObject projectile = Instantiate(Projectile) as GameObject;
-        projectile.transform.parent = ProjectileParent.transform;
+        projectile.transform.parent = _projectileParent.transform;
         projectile.transform.position = Gun.transform.position;
     }
 }
