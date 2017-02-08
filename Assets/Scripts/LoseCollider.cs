3b9b0becedb577ad966b3e97b90b2e9a13215911
Goga Akobiya <googlichek.one@gmail.com>
2017-02-08 10:33:01 +0300

    glitch-garden: Adding lose condition.

---
+++ Assets/Scripts/LoseCollider.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class LoseCollider : MonoBehaviour
+{
+    private LevelManager _levelManager;
+
+    void Start()
+    {
+        _levelManager = FindObjectOfType<LevelManager>();
+    }
+
+    void OnTriggerEnter2D()
+    {
+        _levelManager.LoadLevel("03b Lose");
+    }
+}
